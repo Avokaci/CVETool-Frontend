b204fb2787f84dc807b16b3f486827e60df3af37
Avokaci <if19b123@technikum-wien.at>
2022-03-21 17:47:39 +0100

    test

--- cvetool-frontend/src/app/data-table/data-table.component.ts
+++ cvetool-frontend/src/app/data-table/data-table.component.ts
@@ -25,9 +25,31 @@ export class DataTableComponent implements AfterViewInit {
   columnsToDisplay  = ['cveId', 'cweId', 'vulnerabilityType', 'publishDate', 'updateDate', 'score', 'searchExploit', 'access', 'complexity', 'authentication', 'confidentiality', 'integrity', 'availability' ];
  
   
-  dataSource = new MatTableDataSource<Cve>(ELEMENT_DATA);
+  dataSource = new MatTableDataSource<Cve>();
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+
+  @ViewChild('formYearRange', { static: true }) ngFormYearRange!: NgForm;
+  @ViewChild('formScoreRange', { static: true }) ngFormScoreRange!: NgForm;
+  @ViewChild('formAllCVE', { static: true }) ngFormAllCVE!: NgForm;
+  @ViewChild('formVuln', { static: true }) ngFormVuln!: NgForm;
+  @ViewChild('formAccess', { static: true }) ngFormAccess!: NgForm;
+  @ViewChild('formCompl', { static: true }) ngFormCompl!: NgForm;
+  @ViewChild('formAuth', { static: true }) ngFormAuth!: NgForm;
+  @ViewChild('formConf', { static: true }) ngFormConf!: NgForm;
+  @ViewChild('formInteg', { static: true }) ngFormInteg!: NgForm;
+  @ViewChild('formAvai', { static: true }) ngFormAvai!: NgForm;
+
+  formYearRangeChangesSubscription:any;
+  formScoreRangeChangesSubscription:any;
+  formAllCVEChangesSubscription:any;
+  formVulnChangesSubscription:any;
+  formAccessChangesSubscription:any;
+  formComplChangesSubscription:any;
+  formAuthChangesSubscription:any;
+  formConfChangesSubscription:any;
+  formIntegChangesSubscription:any;
+  formAvaiChangesSubscription:any;
 
   expandedElement: Cve | null = null;
   data: Cve[] =[];
@@ -102,18 +124,53 @@ export class DataTableComponent implements AfterViewInit {
   };
  
 
-  
-
   constructor(public service:CveService) {
     this.onSubmitclearFilters();
     this.ngAfterViewInit();
   }
 
   ngAfterViewInit() {
+   
+   
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
 
   }
+
+  ngOnInit()
+  {
+    this.formYearRangeChangesSubscription = this.ngFormYearRange.form.valueChanges.subscribe(x => {
+      console.log("asd");
+    })
+    this.formScoreRangeChangesSubscription = this.ngFormScoreRange.form.valueChanges.subscribe(x => {
+      console.log("asd");
+    })
+    this.formAllCVEChangesSubscription = this.ngFormAllCVE.form.valueChanges.subscribe(x => {
+      console.log("asd");
+    })
+    this.formVulnChangesSubscription = this.ngFormVuln.form.valueChanges.subscribe(x => {
+      console.log("asd");
+    })
+    this.formAccessChangesSubscription = this.ngFormAccess.form.valueChanges.subscribe(x => {
+      console.log("asd");
+    })
+    this.formComplChangesSubscription = this.ngFormCompl.form.valueChanges.subscribe(x => {
+      console.log("asd");
+    })
+    this.formAuthChangesSubscription = this.ngFormAuth.form.valueChanges.subscribe(x => {
+      console.log("asd");
+    })
+    this.formConfChangesSubscription = this.ngFormConf.form.valueChanges.subscribe(x => {
+      console.log("asd");
+    })
+    this.formIntegChangesSubscription = this.ngFormInteg.form.valueChanges.subscribe(x => {
+      console.log("asd");
+    })
+    this.formAvaiChangesSubscription = this.ngFormAvai.form.valueChanges.subscribe(x => {
+      console.log("asd");
+    })
+  }
+
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
@@ -168,6 +225,7 @@ export class DataTableComponent implements AfterViewInit {
   onSubmitgetAllYearRangeFilteredCVEs(form:NgForm){
     this.service.getAllYearRangeFilteredCVEs(this.yearValue.toString(), this.yearHighValue.toString()).subscribe(
        res =>{
+      
         this.data = res;
         this.dataSource = new MatTableDataSource<Cve>(this.data);
         this.ngAfterViewInit();
@@ -212,6 +270,7 @@ export class DataTableComponent implements AfterViewInit {
  onSubmitgetAccessFilteredCVEs(form:NgForm){
   this.service.getAllFilteredCVEs("Access",this.selectedAccess ).subscribe(
      res =>{
+     
       this.data = res;
       this.dataSource = new MatTableDataSource<Cve>(this.data);
       this.ngAfterViewInit();
@@ -296,9 +355,12 @@ onSubmitgetAvailabilityFilteredCVEs(form:NgForm){
      }
    );
 }
- 
- 
+
+
+
 }
+ 
+ 
 
 
 interface FilterView {
