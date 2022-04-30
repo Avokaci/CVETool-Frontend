6c94f17a4ea86ccc1ccfd12e2fdd620584b0faea
Avokaci <if19b123@technikum-wien.at>
2022-04-30 11:15:58 +0200

    beautifying

--- cvetool-frontend/src/app/app-routing.module.ts
+++ cvetool-frontend/src/app/app-routing.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CVEsComponent } from './home/cves/cves.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'cves', component: CVEsComponent },
-];
+  { path: 'home', component: HomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- cvetool-frontend/src/app/app.module.ts
+++ cvetool-frontend/src/app/app.module.ts
@@ -5,10 +5,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CVEsComponent } from './home/cves/cves.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DataTableComponent } from './data-table/data-table.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
@@ -20,9 +18,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    CVEsComponent,
-    HomeComponent,
-    DataTableComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
